aecf90c74af664152dc14b1accda49e06610b35f
glrizea <123543404+glrizea@users.noreply.github.com>
2023-08-29 09:26:49 +0300

    Update portfolio_project_01.sql

--- portfolio_project_01.sql
+++ portfolio_project_01.sql
@@ -1,9 +1,5 @@
-/*
-Covid 19 Data Exploration 
-
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
-
-*/
+--Covid 19 Data Exploration 
+--Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 
 -- quering the covid deaths data
